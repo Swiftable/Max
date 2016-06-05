3047e60dfaace4820abe347394434d5e3f9e522c
SaidSikira <saidsikira@gmail.com>
2016-06-05 16:39:20 +0200

    Initial Commit

---
+++ Max/Max.h
@@ -0,0 +1,19 @@
+//
+//  Max.h
+//  Max
+//
+//  Created by Said Sikira on 6/5/16.
+//  Copyright © 2016 Said Sikira. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Max.
+FOUNDATION_EXPORT double MaxVersionNumber;
+
+//! Project version string for Max.
+FOUNDATION_EXPORT const unsigned char MaxVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Max/PublicHeader.h>
+
+
